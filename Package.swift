138ed60c926868968bdb965ae2709f8dced2ee2d
Mx-Iris <mxiris@126.com>
2026-05-24 17:49:56 +0800

    Lower minimum macOS deployment target to 10.15

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "MachInjector",
-    platforms: [.macOS(.v11)],
+    platforms: [.macOS(.v10_15)],
     products: [
         .library(
             name: "MachInjector",
